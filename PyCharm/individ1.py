323e38149a042211bbf5329db7a83d85572f05d6
aregdz <144221910+aregdz@users.noreply.github.com>
2023-11-22 12:07:59 +0300

    Update individ1.py

--- PyCharm/individ1.py
+++ PyCharm/individ1.py
@@ -18,74 +18,75 @@ if __name__ == '__main__':
     while True:
         # Запросить команду из терминала.
         command = input(">>> ").lower()
+        
         # Выполнить действие в соответствие с командой.
+        match comand:
+            case 'exit':
+                break
 
-        if command == 'exit':
-            break
+            case 'add':
+            # Запросить данные о работнике.
+                name = input("Название пункта назначения рейса? ")
+                number = int(input("Номер рейса? "))
+                tip = input("Тип самолета? ")
 
-        elif command == 'add':
-        # Запросить данные о работнике.
-            name = input("Название пункта назначения рейса? ")
-            number = int(input("Номер рейса? "))
-            tip = input("Тип самолета? ")
+                # Создать словарь.
+                i = {'name': name, 'number': number, 'tip': tip}
 
-            # Создать словарь.
-            i = {'name': name, 'number': number, 'tip': tip}
+                # Добавить словарь в список.
+                aircrafts.append(i)
 
-            # Добавить словарь в список.
-            aircrafts.append(i)
+                # Отсортировать список в случае необходимости.
+                if len(aircrafts) > 1:
+                    aircrafts.sort(key=lambda item: item.get('name', ''))
 
-            # Отсортировать список в случае необходимости.
-            if len(aircrafts) > 1:
-                aircrafts.sort(key=lambda item: item.get('name', ''))
+            case 'list':
+                # Заголовок таблицы.
+                line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+                '-' * 4,
+                '-' * 30,
+                '-' * 20,
+                '-' * 8
+                )
+                print(line)
 
-        elif command == 'list':
-            # Заголовок таблицы.
-            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
-            '-' * 4,
-            '-' * 30,
-            '-' * 20,
-            '-' * 8
-            )
-            print(line)
+                # Вывести данные о всех сотрудниках.
+                for idx, i in enumerate(aircrafts, 1):
+                    print(
+                        '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
+                            idx,
+                            i.get('name', ''),
+                            i.get('number', ''),
+                            i.get('tip', '')
+                        )
+                )
+                print(line)
 
-            # Вывести данные о всех сотрудниках.
-            for idx, i in enumerate(aircrafts, 1):
-                print(
-                    '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
-                        idx,
-                        i.get('name', ''),
-                        i.get('number', ''),
-                        i.get('tip', '')
-                    )
-            )
-            print(line)
+            case 'select ':
+                # Разбить команду на части для выделения номера года.
+                parts = input("Введите значение: ")
 
-        elif command.startswith('select '):
-            # Разбить команду на части для выделения номера года.
-            parts = command.split(' ', maxsplit=1)
+                # Проверить сведения работников из списка.
+                count = 0
+                for i in aircrafts:
+                    for k, v in i.items():
+                        if v == parts:
+                            print("Пункт назначения - ", i["name"])
+                            print("Номер рейса - ", i["number"])
+                            count += 1
 
-            # Проверить сведения работников из списка.
-            count = 0
-            for i in aircrafts:
-                for k, v in i.items():
-                    if v == parts[1]:
-                        print("Пункт назначения - ", i["name"])
-                        print("Номер рейса - ", i["number"])
-                        count += 1
+                # Если счетчик равен 0, то работники не найдены.
+                if count == 0:
+                    print("Рейс с заданным типом самолёта не найден.")
 
-            # Если счетчик равен 0, то работники не найдены.
-            if count == 0:
-                print("Рейс с заданным типом самолёта не найден.")
+            case 'help':
+                # Вывести справку о работе с программой.
+                print("Список команд:\n")
+                print("add - добавить рейс;")
+                print("list - вывести список рйсов;")
+                print("select <тип> - вывод на экран пунктов назначения и номеров рейсов для данного типа самолёта")
+                print("help - отобразить справку;")
+                print("exit - завершить работу с программой.")
 
-        elif command == 'help':
-            # Вывести справку о работе с программой.
-            print("Список команд:\n")
-            print("add - добавить рейс;")
-            print("list - вывести список рйсов;")
-            print("select <тип> - вывод на экран пунктов назначения и номеров рейсов для данного типа самолёта")
-            print("help - отобразить справку;")
-            print("exit - завершить работу с программой.")
-
-        else:
-            print(f"Неизвестная команда {command}")
+            case _:
+                print(f"Неизвестная команда {command}")
